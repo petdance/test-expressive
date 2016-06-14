d386c9757ada7849e22171019f5038560da177e4
Andy Lester <andy@petdance.com>
2016-06-13 23:42:00 -0500

    Starting on docs

--- lib/Test/Expressive.pm
+++ lib/Test/Expressive.pm
@@ -6,7 +6,7 @@ use warnings;
 
 =head1 NAME
 
-Test::Expressive - The great new Test::Expressive!
+Test::Expressive - Helper functions for writing tests that say what you really mean.
 
 =head1 VERSION
 
@@ -18,6 +18,49 @@ our $VERSION = '0.01';
 
 
 =head1 SYNOPSIS
+
+    use Test::Expressive;
+
+    my @array = get_widgets();
+    is_even( scalar @a, '@array must have an even number of widgets' );
+
+    is_empty_array( \@errors, 'Should have received no errors back' );
+
+    is_nonempty_array( \@warnings, 'Got back at least one warning' );
+
+=head1 WHY TEST::EXPRESSIVE?
+
+Test::Expressive is designed to make your code more readable, based on the
+idea that reading English is easier and less prone to misinterpretation
+than reading Perl, and less prone to error by reducing common cut &
+paste tasks.
+
+Conside either of these two tests:
+
+    ok( $x % 2 == 0 );
+    is( $x % 2, 0 );
+
+What are they doing?  They're testing that C<$x> is an even number.
+It's a common expression that every programmer knows.  Most any programmer
+will see that and think "Aha, it's testing to see if it's an even number."
+
+Better still to make it explicitly clear, in English, what you're trying
+to accomplish:
+
+    is_even( $x );
+
+Test::Expressive also does more stringent checking than the common quick
+tests that we put in.  These tests will all pass.  You probably don't
+want them to.
+
+    for my $x ( undef, 'foo', {}, [] ) {
+        ok( $x % 2 == 0 );
+    }
+
+
+Test::Expressive is based on the idea that the reader should be able to
+tell as much from English as possible without having to decipher code.
+
 
 Quick summary of what the module does.
 
@@ -55,19 +98,18 @@ Andy Lester, C<< <andy at petdance.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-test-expressive at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Expressive>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Please report any bugs or feature requests to
+C<bug-test-expressive at rt.cpan.org>,
+or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Expressive>.
+I will be notified, and then you'll automatically be notified of progress
+on your bug as I make changes.
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc Test::Expressive
-
 
 You can also look for information at:
 
@@ -91,50 +133,16 @@ L<http://search.cpan.org/dist/Test-Expressive/>
 
 =back
 
-
 =head1 ACKNOWLEDGEMENTS
 
+None yet.
 
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2016 Andy Lester.
 
 This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+under the terms of the the Artistic License (2.0).
 
 =cut
 
